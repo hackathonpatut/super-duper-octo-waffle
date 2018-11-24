e65d02e90555ff6eac2b04e66be58d40a90de616
Patrik Marin <patrik.marin@gmail.com>
2018-11-24 09:59:36 +0200

    Add scores

--- client/src/components/Product/index.js
+++ client/src/components/Product/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Item, Card, Image, Dimmer, Loader } from 'semantic-ui-react';
+import { Item, Card, Image, Dimmer, Loader, Statistic, Divider } from 'semantic-ui-react';
 import axios from 'axios';
 
 export default class Product extends Component {
@@ -7,7 +7,9 @@ export default class Product extends Component {
     code: null,
     name: null,
     price: null,
-    segment: null
+    segment: null,
+    sustainability: 10,
+    health: 60,
   };
 
   componentDidMount() {
@@ -32,18 +34,24 @@ export default class Product extends Component {
       });
   }
 
+  scoreToColor = (score) => {
+    if (score < 30) return 'red';
+    if (score < 80) return 'orange';
+    return 'green';
+  }
+
   render() {
-    if (this.state.code === -1) return <p>Tuotetta ei löytynyt</p>;
+    if (this.state.code === -1) return <p>Product not found</p>;
     if (this.state.code === null) return (
       <div className="product-card">
         <Card>
           <div style={{minHeight: '300px'}}>
             <Dimmer active inverted>
-              <Loader>Ladataan...</Loader>
+              <Loader>Loading...</Loader>
             </Dimmer>
           </div>
           <Card.Content>
-            <Card.Header>Ladataan...</Card.Header>
+            <Card.Header>Loading...</Card.Header>
           </Card.Content>
         </Card>
       </div>
@@ -55,17 +63,24 @@ export default class Product extends Component {
           <Image src={`${this.state.image || "http://placehold.it/200x200"}`} />
           <Card.Content>
             <Card.Header>{this.state.name}</Card.Header>
-            <Card.Meta>
+            <Card.Meta className="infotext">
               <span>{`EAN: ${this.props.ean}` || 'EAN: XXX'}</span>
+              <span>{this.state.price} €</span>
             </Card.Meta>
             <Card.Description>
-              <div>
-                Sustainability
-              </div>
+              <Statistic.Group size="medium">
+                <Statistic color={this.scoreToColor(this.state.sustainability)}>
+                  <Statistic.Value>{this.state.sustainability}</Statistic.Value>
+                  <Statistic.Label>Sustainability</Statistic.Label>
+                </Statistic>
+                <Statistic color={this.scoreToColor(this.state.health)}>
+                  <Statistic.Value>{this.state.health}</Statistic.Value>
+                  <Statistic.Label>Health</Statistic.Label>
+                </Statistic>
+              </Statistic.Group>
+              <Divider />
+              <p>How about</p>
             </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            {this.state.price} €
           </Card.Content>
         </Card>
       </div>
