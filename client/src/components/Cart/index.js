1220d8dfd8e02050febf6140f61cafac6baef456
Ville Vuorenmaa <villevuor@gmail.com>
2018-11-24 12:23:52 +0200

    progress bars

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Progress, Button } from 'semantic-ui-react';
 
 export default class Cart extends Component {
   render() {
@@ -9,6 +9,16 @@ export default class Cart extends Component {
 
     return (
       <div>
+        <h3>Your cart</h3>
+        <p><strong>Nicely done, Patu!</strong> Your overall score was 56 % better than your mates. Keep going!</p>
+        <h4>Overall score</h4>
+        <Progress percent={13} color='teal' />
+        <h4>Sustainability score</h4>
+        <Progress percent={23} color='purple' />
+        <p>Produced far away.</p>
+        <h4>Health score</h4>
+        <Progress percent={37} color='olive' />
+        <p>Quite much sugar.</p>
         <ul>
           {items.map((item, i) => <li key={i}>{item.name}, {item.price} €</li>)}
         </ul>
